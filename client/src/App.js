387ccd47c4588f94ebe6ead3d0375147eba4d9cd
Justin King <kingjust@mail.gvsu.edu>
2026-03-07 18:10:12 +0000

    Set up basic frontend structure
    Add base React app structure with React Router and Axios as dependencies.
    Includes a firebase.js config file with placeholder values to be filled
    in once the Firebase project is created.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      {/* Routes will be added here by the frontend team */}
+      <Route path="/" element={<div>StudyPlanShare</div>} />
+    </Routes>
+  );
+}
+
+export default App;
